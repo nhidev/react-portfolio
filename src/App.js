962c028978acdd01cd444fdf07f0f72fecbfe256
nhithai <nhithai88@gmail.com>
2018-10-31 04:33:56 +0700

    optimize source

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
-import LandingPage from '@pages/LandingPage';
-import AboutPage from '@pages/AboutPage';
-import PortfolioPage from '@pages/PortfolioPage';
-// import ContactPage from '@pages/ContactPage';
-import ScrollTop from '@components/ScrollTop';
-import Footer from '@components/Footer';
+import LandingPage from './pages/LandingPage/LandingPage';
+import AboutPage from './pages/AboutPage/AboutPage';
+import PortfolioPage from './pages/PortfolioPage/PortfolioPage';
+import ScrollTop from './components/ScrollTop/ScrollTop';
+import Footer from './components/Footer/Footer';
 
 class App extends Component {
-
   render() {
     return (
       <div className="dev-landing-page">
--- src/pages/AboutPage/AboutPage.js
+++ src/pages/AboutPage/AboutPage.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ScrollToNext from '@components/ScrollToNext';
 import './style.scss';
 
-const AboutPage = (props, context) => {
+const AboutPage = () => {
 
   return (
     <div className="about-page" >
